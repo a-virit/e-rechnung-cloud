259c818cf4832f3147b7e436c4e583170315e190
a-virit <ahmet0511@gmail.com>
2025-09-12 16:57:00 +0200

    SSO V0.02

--- src/components/Settings/ProfileSystemSettings.jsx
+++ src/components/Settings/ProfileSystemSettings.jsx
@@ -5,10 +5,10 @@ import {
   Smartphone, Globe, Database, Activity, Users, CreditCard,
   Download, Upload, Trash2, Copy, RefreshCw, Calendar
 } from 'lucide-react';
-import { useApp } from '../../context/AppContext';
-import MainContacts from './MainContacts';
+import { useApp } from '../../context/AppContext.jsx';
+import MainContacts from './MainContacts.jsx';
 //import SSOConfiguration from './SSOConfiguration';
-import SSOConfig from './SSOConfig';
+import SSOConfig from './SSOConfig.jsx';
 
 
 const ProfileSystemSettings = () => {
